47c9438298b2fd48f938d08a251abe03feff5f93
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-01-08 11:40:59 -0500

    Add final polish to zookeeper::repo

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,10 +1,30 @@
 # == Class zookeeper::repo
 # Configures the Apache Bigtop archive repoisitory.
 #
+# === Parameters
+#
+# [*release*]
+#   The Apache Bigtop release to deploy. Defaults to 0.7.0 current at this time.
+#
+# === Examples
+#
+#  class { 'zookeeper::repo':
+#    release = '0.5.0'
+#  }
+#
+# === Authors
+#
+# Jeremy T. Bouse <jeremy.bouse@undergrid.net>
+#
+# === Copyright
+#
+# Copyright 2014 UnderGrid Network Services
+#
 class zookeeper::repo (
-  $version = '0.7.0'
+  $release = '0.7.0'
 ) {
   $baseurl = 'http://bigtop.s3.amazonaws.com/releases'
+  $gpgkeyurl = 'http://archive.apache.org/dist/bigtop/KEYS'
 
   if $::osfamily == 'RedHat' {
     $dist = $::operatingsystem ? {
@@ -12,12 +32,12 @@ class zookeeper::repo (
       Amazon  => 6,
     }
 
-    yumrepo { "bigtop":
-      baseurl  => "${baseurl}/${version}/redhat/${dist}/${::architecture}",
-      enabled  => true,
-      gpgcheck => true,
-      gpgkey   => 'http://archive.apache.org/dist/bigtop/KEYS',
-      descr    => 'Bigtop'
+    yumrepo { 'bigtop':
+      baseurl  => "${baseurl}/${release}/redhat/${dist}/${::architecture}",
+      enabled  => 1,
+      gpgcheck => 1,
+      gpgkey   => $gpgkeyurl,
+      descr    => "Apache Bigtop El ${dist} - ${::architecture}",
     }
   }
 }
