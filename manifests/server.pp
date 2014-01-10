62fd9531ee1bd158263a48a9a2fdb994c79f51bf
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-01-10 08:35:39 -0500

    Add service resource, resource glue and documentation
    Added the zookeeper-server service resource block
    Wires up notify and subscribe glue between dependent resources
    Updated parameter documentation that had been left empty

--- manifests/server.pp
+++ manifests/server.pp
@@ -5,10 +5,22 @@
 # === Parameters
 #
 # [*ensemble*]
+#   (Array) An array of client certificate names (hostnames) that make up
+#   the Zookeeper ensemble.
+#
 # [*data_dir*]
+#   (String) The path to where Zookeeper will store the in-memory database
+#   snapshots and transaction log of updates to the databases
+#
 # [*client_port*]
+#   (Integer) The port to listen for client connections
+#
 # [*leader_port*]
+#   (Integer) The port followers use to connect to the cluster leader
+#
 # [*election_port*]
+#   (Integer) The port used for leader election if electionAlg is 1, 2 or
+#   3 (default). Not necessary if electionAlg is 0.
 #
 # === Variables
 #
@@ -18,7 +30,10 @@
 # === Examples
 #
 #  class { 'zookeeper::server':
-#    ensemble => [ 'zk1.example.com', 'zk2.example.com', 'zk3.example.com' ],
+#    ensemble      => ['zk1.example.com', 'zk2.example.com', 'zk3.example.com'],
+#    data_dir      => '/opt/zookeeper',
+#    leader_port   => 2182,
+#    election_port => 2183,
 #  }
 #
 # === Authors
@@ -44,15 +59,25 @@ class zookeeper::server (
     require => Package['java']
   }
 
+  service { 'zookeeper-server':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => Package['zookeeper-server'],
+    subscribe  => File['/etc/zookeeper/conf/zoo.cfg'],
+  }
+
   file { '/etc/zookeeper/conf/zoo.cfg':
-    content => template('zookeeper/zoo.cfg.erb'),
-    require => Package['zookeeper-server'],
+    content   => template('zookeeper/zoo.cfg.erb'),
+    require   => Package['zookeeper-server'],
   }
 
   if is_array($ensemble) and member($ensemble, $::clientcert) {
     file { "${data_dir}/myid":
       content => inline_template('<%= ensemble.index(clientcert).to_i.to_s %>'),
       require => Package['zookeeper-server'],
+      notify  => Service['zookeeper-server'],
     }
   }
 }
