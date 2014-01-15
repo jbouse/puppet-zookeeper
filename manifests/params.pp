2571a04f3b7d949aa58c8c16a1bcf0217258ba5f
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-01-15 12:05:57 -0500

    Documentation fix

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,31 +2,6 @@
 #
 # Apache Zookeeper default parameters
 #
-# === Variables
-#
-# [*ensemble*]
-#   (Array) An array of client certificate names (hostnames) that make up
-#   the Zookeeper ensemble.
-#   Default: undefined
-#
-# [*data_dir*]
-#   (String) The path to where Zookeeper will store the in-memory database
-#   snapshots and transaction log of updates to the databases
-#   Default: /var/lib/zookeeper
-#
-# [*client_port*]
-#   (Integer) The port to listen for client connections
-#   Default: 2181
-#
-# [*leader_port*]
-#   (Integer) The port followers use to connect to the cluster leader
-#   Default: 2888
-#
-# [*election_port*]
-#   (Integer) The port used for leader election if electionAlg is 1, 2 or
-#   3 (default). Not necessary if electionAlg is 0.
-#   Default: 3888
-#
 # === Authors
 #
 # Jeremy T. Bouse <jeremy.bouse@undergrid.net>
